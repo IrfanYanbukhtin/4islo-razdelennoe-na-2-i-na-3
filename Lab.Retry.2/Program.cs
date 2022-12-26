8e661a3ce30b609c309de07c80c3ea2499887621
Irfan Yanbukhtin <woolfyko@gmail.com>
2022-12-26 18:53:51 +0400

    Add project files.

---
+++ Lab.Retry.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab.Retry._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ededi daxil edin:");
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 2 == 0 && i % 3 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine($"Cem: {sum}");
+        }
+    }
+}
